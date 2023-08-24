f01e87df606e661774764ce80ec05e6a1ea892f4
Volodymyr Marchuk <mr.volodymyr.marchuk@gmail.com>
2023-08-24 22:10:52 +0200

    added login

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { App } from "./App";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux";
-// import { PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from "redux-persist/integration/react";
 
 import "./styles/base.scss";
 import { Loader } from "./components/Loader/Loader";
@@ -11,11 +11,11 @@ import { Loader } from "./components/Loader/Loader";
 ReactDOM.render(
   <StrictMode>
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-      <Suspense fallback={<Loader />}>
-        <App />
-      </Suspense>
-      {/* </PersistGate> */}
+      <PersistGate loading={null} persistor={persistor}>
+        <Suspense fallback={<Loader />}>
+          <App />
+        </Suspense>
+      </PersistGate>
     </Provider>
   </StrictMode>,
   document.getElementById("root")
